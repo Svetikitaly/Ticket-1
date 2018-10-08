855b6641bb2e029f428e7bebd7f7d5c0e17b5c61
Svetikitaly <43605187+Svetikitaly@users.noreply.github.com>
2018-10-08 16:48:00 +0200

    Update 1.js
    Модифицировать код, сделать username, email, password приватными методами, т.е получить доступ (прочитать. изменить. удалить переменную) снаружи нельзя, но нужно реализовать публичные методы getUsername getEmail getPassword которые вернут нужные данные.
    c = new C(username, email, password);
    c.password // undefined
    c.getPassword() // password

--- 1.js
+++ 1.js
@@ -1,11 +1,15 @@
 
 function C(username, email, password) {
+  this.getUsername = () => console.log( username);
+  this.getEmail = () => console.log( email );
+  this.getPassword = () => console.log( password);
+  this.getHashPassword = function() {
     var sha256 = require('js-sha256');
-    this.getHashPassword = function() {
-        console.log(sha256( password ));
-      };
+    console.log(sha256( password ));
+  };
 }
 
 
 var c = new C('sveta', 'svettikomsk', '18121987m');
 c.getHashPassword();
+c.getPassword ();
