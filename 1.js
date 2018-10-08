0aa6df91e1ab043711a531b7fabd1f50031328a5
Svetikitaly <43605187+Svetikitaly@users.noreply.github.com>
2018-10-08 16:50:18 +0200

    Update 1.js
    Тело функции getHashPassword должно выглядеть как:
    getPassword = function(){
            return _getHashPassword()
    }
    где _getHashPassword приватный метод в котором скрыта реализация использованного алгоритма хэширования

--- 1.js
+++ 1.js
@@ -1,15 +1,19 @@
 
 function C(username, email, password) {
-  this.getUsername = () => console.log( username);
-  this.getEmail = () => console.log( email );
-  this.getPassword = () => console.log( password);
-  this.getHashPassword = function() {
+    this.getUsername = () => console.log( username);
+    this.getEmail = () => console.log( email );
+    this.getPassword = () => console.log ( password );
+    this.getHashPassword = function() {
+      return _getHashPassword();
+    } 
+  
     var sha256 = require('js-sha256');
-    console.log(sha256( password ));
+  
+    function _getHashPassword () {
+    console.log( sha256( password ) );
   };
 }
 
 
 var c = new C('sveta', 'svettikomsk', '18121987m');
-c.getHashPassword();
-c.getPassword ();
+c.getHashPassword ();
