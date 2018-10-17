2fba5e3a59ab6ce201a15e14c4eb0b2f2598bb88
Svetikitaly <43605187+Svetikitaly@users.noreply.github.com>
2018-10-17 15:53:25 +0200

    Update 1.js
    При помощи jest написать тесты проверяющие работоспособность getPassword, getUsername и т.д

--- 1.js
+++ 1.js
@@ -1,19 +1,15 @@
 
+var sha256 = require('js-sha256');
+
 function C(username, email, password) {
-    this.getUsername = () => console.log( username);
-    this.getEmail = () => console.log( email );
-    this.getPassword = () => console.log ( password );
-    this.getHashPassword = function() {
-      return _getHashPassword();
-    } 
-  
-    var sha256 = require('js-sha256');
-  
+    this.getUsername = () =>  username;
+    this.getEmail = () =>  email ;
+    this.getPassword = () =>  password ;
+    this.getHashPassword = () => _getHashPassword();
+    
     function _getHashPassword () {
-    console.log( sha256( password ) );
+    return sha256( password ) ;
   };
 }
 
-
-var c = new C('sveta', 'svettikomsk', '18121987m');
-c.getHashPassword ();
+module.exports = C;
