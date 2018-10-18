ce4c1980308021bc5384eaaf3277f6c1a12bb39c
Svetikitaly <43605187+Svetikitaly@users.noreply.github.com>
2018-10-18 16:29:46 +0200

    Update 1.test.js
    6.Написать тест

--- 1.test.js
+++ 1.test.js
@@ -1,14 +1,23 @@
-var C = require('./1');
-var assertEqual = require('assert-equal')
+var C = require('./6');
+var assertEqual = require('assert-equal' ), sha256 = require('js-sha256');
+
 describe('test', () => {
-  var c = new C('sveta', 'svettikomsk', '18121987m');
-  it('user name', () => {
-    assertEqual(c.getUsername(), 'sveta');
+ var test_name = 'sveta', test_email = 'svettikomsk', test_password = '18121987m', test_iter_count = 7;
+ var c = new C( test_name, test_email, test_password, test_iter_count);
+    it('user name', () => {
+      assertEqual(c.getUsername(), test_name);
   });
   it('email', () => {
-    assertEqual(c.getEmail(), 'svettikomsk');
+      assertEqual(c.getEmail(), test_email);
   });
   it('password', () => {
-    assertEqual(c.getPassword(), '18121987m');
+      assertEqual(c.getPassword(), test_password);
+  });
+  it('Hash Password', () => {
+      var test_hash = sha256( test_password ) ;
+      for (i = 0; i < ( test_iter_count - 1 ); i++) { 
+        test_hash = sha256( test_hash ) ;
+      }
+      assertEqual(c.getHashPassword(), test_hash);
   });
 });
